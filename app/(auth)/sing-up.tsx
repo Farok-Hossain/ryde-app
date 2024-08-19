c11d498df9a281637f06836b5e8d0e505a4dd20b
Farok Hossain <farokafs7@gmail.com>
2024-08-19 11:25:48 +0600

    added singIn signUp

---
+++ app/(auth)/sing-up.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SingUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SingUp</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SingUp;
