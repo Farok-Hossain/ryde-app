d3046d68bee5154844089abdba60fc4a730e20d3
Farok Hossain <farokafs7@gmail.com>
2024-08-20 22:17:12 +0600

    updated

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,11 +1,12 @@
 import { router } from "expo-router";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <SafeAreaView className="flex h-full bg-white items-center justify-between">
       <TouchableOpacity
@@ -19,7 +20,13 @@ const Onboarding = () => {
       <Swiper
         ref={swiperRef}
         loop={false}
-        dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0]" />}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
       ></Swiper>
     </SafeAreaView>
   );
