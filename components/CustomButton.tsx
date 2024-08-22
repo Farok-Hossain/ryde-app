1419e6803f8c5ac636ce172e6b08ce825bc77aea
Farok Hossain <farokafs7@gmail.com>
2024-08-22 19:11:22 +0600

    updated custombutton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,6 +5,14 @@ const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
   switch (variant) {
     case "secondary":
       return "bg-gray-500";
+    case "danger":
+      return "bg-red-500";
+    case "success":
+      return "bg-green-500";
+    case "outline":
+      return "bg-transparent border-neutral-300 border-[0.5px]";
+    default:
+      return "bg-[#0286ff]";
   }
 };
 
