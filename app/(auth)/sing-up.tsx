284d1012febf5e2e0489c10f22cf1c61ce2b097e
Farok Hossain <farokafs7@gmail.com>
2024-08-22 21:16:52 +0600

    updated

--- app/(auth)/sing-up.tsx
+++ app/(auth)/sing-up.tsx
@@ -5,8 +5,11 @@ const SingUp = () => {
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
-        <View>
+        <View className="relative w-full h-[250px]">
           <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-black text-2xl font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
         </View>
       </View>
     </ScrollView>
