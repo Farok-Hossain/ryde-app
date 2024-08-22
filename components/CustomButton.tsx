361e68b2132a4dba10d6e7fed466ba1f4ef08623
Farok Hossain <farokafs7@gmail.com>
2024-08-22 19:24:53 +0600

    updated again

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -15,6 +15,20 @@ const getBgVariantStyle = (variant: ButtonProps["bgVariant"]) => {
       return "bg-[#0286ff]";
   }
 };
+const getTextVariantStyle = (variant: ButtonProps["textVariant"]) => {
+  switch (variant) {
+    case "primary":
+      return "text-black";
+    case "secondary":
+      return "text-gray-100";
+    case "danger":
+      return "text-red-100";
+    case "success":
+      return "text-green-100";
+    default:
+      return "text-white";
+  }
+};
 
 const CustomButton = ({
   onPress,
@@ -25,7 +39,7 @@ const CustomButton = ({
   IconRight,
   className,
   ...props
-}) => (
+}: ButtonProps) => (
   <TouchableOpacity
     onPress={onPress}
     className={`w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 ${getBgVariantStyle(
@@ -33,7 +47,9 @@ const CustomButton = ({
     )} ${className}`}
   >
     {IconLeft && <IconLeft />}
-    <Text>{title}</Text>
+    <Text className={`text-lg font-bold ${getTextVariantStyle(textVariant)}`}>
+      {title}
+    </Text>
     {IconRight && <IconRight />}
   </TouchableOpacity>
 );
