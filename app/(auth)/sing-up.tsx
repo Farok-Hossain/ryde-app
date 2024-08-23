00f821439b2edf09666dde9b92ba86efce3eea80
Farok Hossain <farokafs7@gmail.com>
2024-08-23 16:30:46 +0600

    updated

--- app/(auth)/sing-up.tsx
+++ app/(auth)/sing-up.tsx
@@ -1,10 +1,15 @@
+import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import { icons, images } from "@/constants";
+import { Link } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
 const SingUp = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+  const onSignUpPress = async () => {};
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -37,7 +42,24 @@ const SingUp = () => {
             value={form.password}
             onChangeText={(value) => setForm({ ...form, password: value })}
           />
+
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+
+          {/* OAuth */}
+          <Link
+            href="/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account?</Text>
+            <Text className="text-primary-500"> Log In</Text>
+          </Link>
         </View>
+
+        {/* Verification Modal */}
       </View>
     </ScrollView>
   );
