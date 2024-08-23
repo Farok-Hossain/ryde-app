8b1d10422df477162ec92caa945d1c26b6c9124f
Farok Hossain <farokafs7@gmail.com>
2024-08-23 19:53:19 +0600

    sign-in component updated

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,62 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
-const SignIn = () => {
+const SingIn = () => {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const onSignInPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-black text-2xl font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+          <Link
+            href="/sing-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Don't have an account?</Text>
+            <Text className="text-primary-500"> Sign Up</Text>
+          </Link>
+        </View>
+
+        {/* Verification Modal */}
+      </View>
+    </ScrollView>
   );
 };
 
-export default SignIn;
+export default SingIn;
