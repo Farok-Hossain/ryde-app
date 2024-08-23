a91e662c5120dab3c99cdbde3686d2e2e98aa818
Farok Hossain <farokafs7@gmail.com>
2024-08-23 16:13:08 +0600

    updated again

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,6 +1,10 @@
+import { InputFieldProps } from "@/types/type";
 import {
+  Image,
+  Keyboard,
   KeyboardAvoidingView,
   Text,
+  TextInput,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
@@ -15,13 +19,25 @@ const InputField = ({
   iconStyle,
   className,
   ...props
-}) => (
+}: InputFieldProps) => (
   <KeyboardAvoidingView>
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View className="my-2 w-full">
         <Text className={`text-lg font-JakartaSemiBold mb-3 ${labelStyle}`}>
           {label}
         </Text>
+        <View
+          className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+        >
+          {icon && (
+            <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
+          )}
+          <TextInput
+            className={`rounded-full p-4 font-JakartaSemiBold text-[15px] flex-1 ${inputStyle} text-left`}
+            secureTextEntry={secureTextEntry}
+            {...props}
+          />
+        </View>
       </View>
     </TouchableWithoutFeedback>
   </KeyboardAvoidingView>
