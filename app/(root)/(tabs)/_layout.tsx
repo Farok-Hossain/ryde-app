8b5c29dea6e0f5e9c2fc862948378acff723365a
Farok Hossain <farokafs7@gmail.com>
2024-09-05 23:25:07 +0600

    home & layout updated

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,30 @@
+import { icons } from "@/constants";
 import { Tabs } from "expo-router";
-import { Image, View } from "react-native";
+import { Image, ImageSourcePropType, View } from "react-native";
 
-const TabIcon = () => (
-  <View>
-    <View>
-      <Image />
+const TabIcon = ({
+  source,
+  focused,
+}: {
+  source: ImageSourcePropType;
+  focused: boolean;
+}) => (
+  <View
+    className={`flex flex-row justify-center items-center rounded-full ${
+      focused ? "bg-general-300" : ""
+    }`}
+  >
+    <View
+      className={`rounded-full w-12 h-12 items-center ${
+        focused ? "bg-general-400" : ""
+      }`}
+    >
+      <Image
+        source={source}
+        tintColor="white"
+        resizeMode="contain"
+        className="w-7 h-7"
+      />
     </View>
   </View>
 );
@@ -12,14 +32,64 @@ const TabIcon = () => (
 const Layout = () => (
   <Tabs
     initialRouteName="index"
-    screenOptions={{ tabBarActiveTintColor: "white" }}
+    screenOptions={{
+      tabBarActiveTintColor: "white",
+      tabBarInactiveTintColor: "white",
+      tabBarShowLabel: false,
+      tabBarStyle: {
+        backgroundColor: "#333333",
+        borderRadius: 50,
+        paddingBottom: 0,
+        overflow: "hidden",
+        marginHorizontal: 20,
+        marginBottom: 20,
+        height: 78,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        position: "absolute",
+      },
+    }}
   >
     <Tabs.Screen
       name="home"
       options={{
         title: "Home",
         headerShown: false,
-        tabBarIcon: () => <TabIcon />,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.home} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="rides"
+      options={{
+        title: "Rides",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.list} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="chat"
+      options={{
+        title: "Chat",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.chat} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="profile"
+      options={{
+        title: "Porfile",
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon focused={focused} source={icons.profile} />
+        ),
       }}
     />
   </Tabs>
--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,23 +1,15 @@
-import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { SignedIn, useUser } from "@clerk/clerk-expo";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Page() {
   const { user } = useUser();
 
   return (
-    <View>
+    <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
-      <SignedOut>
-        <Link href="/sign-in">
-          <Text>Sign In</Text>
-        </Link>
-        <Link href="/sign-up">
-          <Text>Sign Up</Text>
-        </Link>
-      </SignedOut>
-    </View>
+    </SafeAreaView>
   );
 }
