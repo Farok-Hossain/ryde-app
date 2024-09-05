cc1f0f09bcaee367c6af87ac81617f9210a4fd36
Farok Hossain <farokafs7@gmail.com>
2024-09-05 23:02:07 +0600

    updated

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,7 +1,28 @@
 import { Tabs } from "expo-router";
+import { Image, View } from "react-native";
 
-const Layout = () => {
-  <Tabs></Tabs>;
-};
+const TabIcon = () => (
+  <View>
+    <View>
+      <Image />
+    </View>
+  </View>
+);
+
+const Layout = () => (
+  <Tabs
+    initialRouteName="index"
+    screenOptions={{ tabBarActiveTintColor: "white" }}
+  >
+    <Tabs.Screen
+      name="home"
+      options={{
+        title: "Home",
+        headerShown: false,
+        tabBarIcon: () => <TabIcon />,
+      }}
+    />
+  </Tabs>
+);
 
 export default Layout;
