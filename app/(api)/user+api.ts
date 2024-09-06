dfb8aee11560ebbd4468b7ffb3a5ac5f9e06dab2
Farok Hossain <farokafs7@gmail.com>
2024-09-06 06:44:53 +0600

    user+api route created

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,5 +1,32 @@
+import { data } from "@/constants";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(`${process.env.DATABASE_URL}`);
+export async function POST(request: Request) {
+  try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const { name, email, clerkId } = await request.json();
 
-const posts = await sql("SELECT * FROM posts");
+    if (!name || !email || !clerkId) {
+      return Response.json(
+        { error: "Missing required fields " },
+        { status: 400 }
+      );
+    }
+
+    const response = await sql`
+    INSERT INTO users (
+    name,
+    email, clerk_id
+    )
+    VALUES( 
+    ${name},
+    ${email},
+    ${clerkId}
+    )
+    `;
+
+    return new Response(JSON.stringify({ data: response }), { status: 201 });
+  } catch (error) {
+    return Response.json({ error: error }, { status: 500 });
+  }
+}
