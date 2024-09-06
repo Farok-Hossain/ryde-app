912f6a0a78c512c7dd800cf98a96cc250a888fbe
Farok Hossain <farokafs7@gmail.com>
2024-09-06 16:20:48 +0600

    RideCard updated

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -1,9 +1,27 @@
 import { Ride } from "@/types/type";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
-const RideCard = ({ ride }: { ride: Ride }) => (
-  <View>
-    <Text className="text-3xl">{ride.driver.first_name}</Text>
+const RideCard = ({
+  ride: {
+    destination_longitude,
+    destination_latitude,
+    origin_address,
+    destination_address,
+    created_at,
+    ride_time,
+    driver,
+    payment_status,
+  },
+}: {
+  ride: Ride;
+}) => (
+  <View className="flex flex-row items-center justify-center bg-white rounded-lg shadow-sm shadow-neutral-300 mb-3">
+    <View className="flex flex-row items-center justify-between p-3">
+      <View className="flex flex-row items-center justify-between">
+        <Image source={{ uri: `https://maps.geoapify` }} />
+      </View>
+    </View>
+    <Text className="text-3xl">{driver.first_name}</Text>
   </View>
 );
 
