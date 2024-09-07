16aa787461705bfc1cc55aeacdc1a54b843b5a34
Farok Hossain <farokafs7@gmail.com>
2024-09-07 09:51:53 +0600

    RideCard Updated

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -61,6 +61,28 @@ const RideCard = ({
             {driver.first_name} {driver.last_name}
           </Text>
         </View>
+
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-gray-500 ">
+            Car Seats
+          </Text>
+          <Text className="text-md font-JakartaMedium text-gray-500 ">
+            {driver.car_seats}
+          </Text>
+        </View>
+
+        <View className="flex flex-row items-center w-full justify-between mb-5">
+          <Text className="text-md font-JakartaMedium text-gray-500 ">
+            Payment Status
+          </Text>
+          <Text
+            className={`text-md font-JakartaMedium text-gray-500 ${
+              payment_status === "paid" ? "text-green-500" : "text-red-500"
+            }`}
+          >
+            {payment_status}
+          </Text>
+        </View>
       </View>
     </View>
   </View>
